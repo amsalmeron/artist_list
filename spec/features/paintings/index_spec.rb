1f63fb63ee30b3e76237f76168c97078e2c5085e
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-03-28 14:38:39 -0700

    complete user story 18

--- spec/features/paintings/index_spec.rb
+++ spec/features/paintings/index_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe 'painting index' do
     painting_2 = Painting.create!(name: "The Weeping Woman", year_finished: 1937, abstract: true, artist_id: artist_1.id)
     painting_3 = Painting.create!(name: "Guernica", year_finished: 1937, abstract: true, artist_id: artist_1.id)
     painting_4 = Painting.create!(name: "The Snail", year_finished: 1953, abstract: true, artist_id: artist_2.id)
-
     visit '/paintings'
 
     expect(page).to have_no_content(painting_1.name)
@@ -24,8 +23,28 @@ RSpec.describe 'painting index' do
     expect(page).to have_content(painting_3.name)
     expect(page).to have_content(painting_3.year_finished)
     expect(page).to have_content(painting_3.abstract)
+  end
 
-
+  it 'displays link to edit individual painting attributes' do
+    artist_1 = Artist.create!(name: "Picasso", birth_year: 1881, living: false)
+    artist_2 = Artist.create!(name: "Matisse", birth_year: 1869, living: false)
+    artist_3 = Artist.create!(name: "Beeple", birth_year: 1981, living: true)
+    painting_1 = Painting.create!(name: "The Old Guitarist", year_finished: 1904, abstract: true, artist_id: artist_1.id)
+    painting_2 = Painting.create!(name: "The Weeping Woman", year_finished: 1937, abstract: true, artist_id: artist_1.id)
+    painting_3 = Painting.create!(name: "Guernica", year_finished: 1937, abstract: true, artist_id: artist_1.id)
+    painting_4 = Painting.create!(name: "The Snail", year_finished: 1953, abstract: true, artist_id: artist_2.id)
+    visit "/paintings"
+    click_link "Edit #{painting_1.name}"
+    expect(current_path).to eq("/paintings/#{painting_1.id}/edit")
+    visit "/paintings"
+    click_link "Edit #{painting_2.name}"
+    expect(current_path).to eq("/paintings/#{painting_2.id}/edit")
+    visit "/paintings"
+    click_link "Edit #{painting_3.name}"
+    expect(current_path).to eq("/paintings/#{painting_3.id}/edit")
+    visit "/paintings"
+    click_link "Edit #{painting_4.name}"
+    expect(current_path).to eq("/paintings/#{painting_4.id}/edit")
   end
 
 end
